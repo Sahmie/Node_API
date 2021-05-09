a3360590e9b6a0d15a3258c1411b255cfa4c00db
sahmie <sahmienwoko@gmail.com>
2021-05-09 09:14:51 +0100

    background workers

--- index.js
+++ index.js
@@ -4,109 +4,22 @@
  */
 
 // Dependencies
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const fs = require('fs');
-const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./lib/config');
-const handlers = require('./lib/handlers');
-const helpers = require('./lib/helpers');
+const server = require('./lib/server');
+const workers = require('./lib/workers');
 
-// @TODO get rid of this
-helpers.sendTwilioSms('08103032605', 'hello!', (err) => {
-  console.log('this is the err ', err);
-});
+// Declare the app
+const app = {};
 
-//instantiate HTTP server
-const httpServer = http.createServer((req, res) => {
-  unifiedServer(req, res);
-});
-
-// Start the HTTP server
-httpServer.listen(config.httpPort, () => {
-  console.log('The server is listening on port ' + config.httpPort);
-});
-
-//instantiate HTTPS server
-const httpsSeverOptions = {
-  key: fs.readFileSync('./https/key.pem'),
-  cert: fs.readFileSync('./https/cert.pem'),
+// Init function
+app.init = () => {
+  // STart the server
+  server.init();
+  // start the workers
+  workers.init();
 };
 
-const httpsServer = https.createServer(httpsSeverOptions, (req, res) => {
-  unifiedServer(req, res);
-});
+// Execute
+app.init();
 
-// Start the server
-httpsServer.listen(config.httpsPort, () => {
-  console.log('The server is listening on port ' + config.httpsPort);
-});
-
-//All thee server logic for both http and https server
-const unifiedServer = (req, res) => {
-  // Get the URL and parse it
-  const parsedUrl = url.parse(req.url, true);
-
-  // Get the path
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-  // Get the query string as an object
-  const queryStringObject = parsedUrl.query;
-
-  // Get the HTTP Method
-  const method = req.method.toLocaleLowerCase();
-
-  // Get the headers as an object
-  const headers = req.headers;
-
-  // Get the payload, if any
-  const decoder = new StringDecoder('utf-8');
-  let buffer = '';
-  req.on('data', function (data) {
-    buffer += decoder.write(data);
-  });
-
-  req.on('end', function () {
-    buffer += decoder.end();
-
-    // Choose the handler this request should go to. if one is not found use the 404 handler
-    const choosenHandler = Object.keys(router).includes(trimmedPath)
-      ? router[trimmedPath]
-      : handlers.notFound;
-
-    // Construct the data object to send to the handler
-    const data = {
-      trimmedPath,
-      queryStringObject,
-      method,
-      headers,
-      payload: helpers.parseJsonToObject(buffer),
-    };
-
-    // Route the request to the handler specified in the router
-    choosenHandler(data, function (statusCode, payload) {
-      // Use the status code called bck by the handler, or default to 200
-      statusCode = typeof statusCode === 'number' ? statusCode : 200;
-
-      // Use the payload called back or default to an empty object
-      payload = typeof payload === 'object' ? payload : {};
-
-      // Convert the payload to a string
-      const payloadString = JSON.stringify(payload);
-
-      // Return the response
-      res.setHeader('Content-Type', 'application/json');
-      res.writeHead(statusCode);
-      res.end(payloadString);
-    });
-  });
-};
-
-const router = {
-  ping: handlers.ping,
-  users: handlers.users,
-  tokens: handlers.tokens,
-  checks: handlers.checks,
-};
+// Export the app
+module.exports = app;
